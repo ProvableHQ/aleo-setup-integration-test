e4afec7ec70b30264a79bf42150b26e2ee57da2d
Luke Frisken <l.frisken@gmail.com>
2021-07-13 22:18:04 +0400

    allow running a single test by specified test id

--- src/options.rs
+++ src/options.rs
@@ -5,6 +5,8 @@ use std::path::PathBuf;
 use serde::Serialize;
 use structopt::StructOpt;
 
+use crate::specification::TestId;
+
 /// Command line options for running the Aleo Setup integration test.
 /// More complex options (such as drops) are available via the `multi`
 /// command interface by specifying the test in `ron` format.
@@ -16,4 +18,8 @@ use structopt::StructOpt;
 pub struct CmdOptions {
     /// ron file specifying the test options.
     pub specification_file: PathBuf,
+    /// Test with only a specific test id contained within the
+    /// specification file.
+    #[structopt(long = "id")]
+    pub id: Option<TestId>,
 }
